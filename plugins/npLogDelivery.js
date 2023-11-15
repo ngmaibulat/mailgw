354b4ebc66ebf0ec18cf6dacb31a8e140640b15d
ngm <ngmaibulat@gmail.com>
2023-11-15 16:01:13 +0400

    Show version in Logs

--- plugins/npLogDelivery.js
+++ plugins/npLogDelivery.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 // const fetch = require("node-fetch");
 const functions = require("./functions");
 
-const version = "0.0.16";
+const version = "0.0.17";
 
 const logfile = "./log/logDelivery.log";
 
